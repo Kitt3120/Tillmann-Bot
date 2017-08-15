eefa54a28d9844073735f863f5ce08fce65c5831
kitt3120 <Kitt3120@aol.de>
2017-08-15 14:20:55 +0200

    Updated AdminChecker

--- src/main/java/de/vanillekeks/tillmannbot/misc/AdminChecker.java
+++ src/main/java/de/vanillekeks/tillmannbot/misc/AdminChecker.java
@@ -2,15 +2,18 @@ package de.vanillekeks.tillmannbot.misc;
 
 import net.dv8tion.jda.core.entities.User;
 
+import java.util.ArrayList;
+
 public class AdminChecker {
 
-    private static String papstId = "256510610165071873";
-    private static String vanId = "186105158076203011";
-    private static String kittId = "238645059329720330";
-    private static String semihId = "292269732873240597";
-
     public static boolean isAdmin(User user) {
-        return (user.getId().equals(papstId) || user.getId().equals(vanId) || user.getId().equals(kittId) || user.getId().equals(semihId));
+        ArrayList<String> adminIds = new ArrayList<>();
+        adminIds.add("256510610165071873"); //Papst
+        adminIds.add("186105158076203011"); //Van <3
+        adminIds.add("238645059329720330"); //Kitt3120
+        adminIds.add("292269732873240597"); //Semih
+        adminIds.add("325752779106811904"); //Luca
+        return (adminIds.contains(user.getId()));
     }
 
 }
